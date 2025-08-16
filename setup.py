7b3afebf9e5e736e746a35c5f131e3f2da658807
itsMahan <ataeifarmahan@gmail.com>
2025-08-16 07:10:27 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='fixer-demo',
+    version='0.1',
+    description='Fixer service Demo package',
+    url='https://github.com/itsMahan/fixer-demo.git', #github repo
+    author='Mahan',
+    author_email='ataeifarmahan@gmail.com',
+    license='MIT',
+    packages=['fixer'],
+    zip_safe=False,
+)
