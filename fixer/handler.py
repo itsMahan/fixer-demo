e3b9068e6e98a0e575768e0e2474e3d709d21120
itsMahan <ataeifarmahan@gmail.com>
2025-08-16 07:42:11 -0400

    Bump to version 0.2

--- fixer/handler.py
+++ fixer/handler.py
@@ -1,2 +1,13 @@
-def get_rates():
-    print("Hello from get_rates")
+import requests
+import json
+
+
+
+BASE_PATH = 'https://data.fixer.io/api/latest?access_key='
+
+
+def get_rates(api_key):
+    response = requests.get(BASE_PATH + api_key)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return None
--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 
 setup(
     name='fixer-demo',
-    version='0.1',
+    version='0.2',
     description='Fixer service Demo package',
     url='https://github.com/itsMahan/fixer-demo.git', #github repo
     author='Mahan',
     author_email='ataeifarmahan@gmail.com',
     license='MIT',
     packages=['fixer'],
+    install_requires=['requests'],
     zip_safe=False,
 )
